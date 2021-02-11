93d3f3fc1b80ce3c27aa152692234d748b35b933
augustine d. nguyen <augustine.d.nguyen@outlook.com>
2021-02-11 19:17:36 +0700

    add items, pipelines

--- ahazonescrawler/pipelines.py
+++ ahazonescrawler/pipelines.py
@@ -7,6 +7,8 @@
 # useful for handling different item types with a single interface
 import ntpath
 import logging
+import croniter
+import datetime
 from urllib.parse import urljoin
 from itemadapter import ItemAdapter
 from scrapy import Request
@@ -101,3 +103,50 @@ class AhaMangaInfoCrawlerStatusPipeline:
             }, merge=True)
         else:
             logger.error('the inst_id is none')
+
+class AhaMangaChapterDataPipeline:
+    root_collection = 'aha_manga'
+    chapters_collection = 'chapters'
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        manga_id = get_str(adapter['manga_id'])
+        lang_ver = get_str(adapter['lang_version'])
+        chapters = adapter['chapters']
+        if type(chapters) is not list:
+            raise DropItem('invalid [chapters] item')
+        chapters_ref = db_client.collection(self.root_collection).document(manga_id).collection(self.chapters_collection)
+
+        for chapter in chapters:
+            chapter_id = chapter['chapter_id']
+            chapter_url = chapter['chapter_url']
+            chapter_doc = {}
+            if lang_ver == 'en':
+                chapter_doc['en_version'] = chapter_url
+            elif lang_ver == 'ja':
+                chapter_doc['ja_version'] = chapter_url
+            elif lang_ver == 'vi':
+                chapter_doc['vi_version'] = chapter_url
+            else:
+                logger.warn(f'invalid chapter version.')
+                raise DropItem('the lang_version is invalid.')
+            chapters_ref.document(chapter_id).set(chapter_doc, merge=True)
+
+class AhaMangaChapterCrawlerStatusPipeline:
+    root_collection = 'aha_crawler'
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        chapters = adapter['chapters']
+        last_chapter = chapters[-1]['chapter_id']
+
+        inst_id = get_str(adapter['inst_id'])
+        cron_time = get_str(adapter['cron_time'])
+        now = datetime.datetime.now()
+        cron = croniter.croniter(cron_time, now)
+        next_time = cron.get_next(datetime.datetime)
+
+        db_client.collection(self.root_collection).document(inst_id).set({
+            u'next_time': next_time,
+            u'last_chapter': last_chapter
+        }, merge=True)
